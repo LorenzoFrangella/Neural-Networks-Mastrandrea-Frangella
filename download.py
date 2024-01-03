e6eca51fecb42af76219c2cd6ea9bd5f396051df
Pigiby <pgb_2000@outlook.it>
2024-01-03 12:04:38 +0100

    push

--- download.py
+++ download.py
@@ -1,17 +1,17 @@
-import json
- 
-# Opening JSON file
-f = open('ontology.json')
- 
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
+from  pytube import YouTube
 
-for i in range(len(data)):
-    print(data[i]["child_ids"])
+import os
 
+video_id = "--4gqARaEJE"
 
+video_url = f"https://www.youtube.com/watch?v={video_id}"
 
+selected_video = YouTube(video_url)
+audio = selected_video.streams.filter(only_audio = True).first()
 
-# Closing file
-f.close()
+destination = "./download"
+
+base , ext = os.path.splitext(destination)
+new_file = base + '.mp3'
+os.rename(destination,new_file)
+print(type(audio))
